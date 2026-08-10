61cae16f088023faf25bd976dfbad480ee0bceca
Paul Thanksgiving <pthanksdev@gmail.com>
2026-08-10 03:43:33 -0600

    Add initial types package with configuration and exports

---
+++ packages/ledger-client/src/index.ts
@@ -0,0 +1,2 @@
+export { LedgerClient, PostTransactionRequest, BalanceResponse, LedgerAccountResponse, TransactionResponse } from './ledger.client';
+export { LedgerModule, LedgerModuleOptions } from './ledger.module';
